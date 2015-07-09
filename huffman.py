8fd61164e2a46ede4209dcca27d2f7c786553e1b
Bao Vuong <vqbao@lv5.org>
2015-07-09 00:26:11 -0400

    more implementation

--- huffman.py
+++ huffman.py
@@ -4,6 +4,10 @@ class Node:
         self.left = left
         self.right = right
         self.position = ''
+    
+    def is_leaf(self):
+        return self.left == self.right == None
+            
 
 def inorder(node):
     if node == None:
@@ -20,12 +24,10 @@ def create_huffman_tree(values):
     while len(nodes) > 1:
         nodes = sorted(nodes,key=lambda node: node.value[0])
         root = Node((nodes[0].value[0]+nodes[1].value[0],'*'),nodes[0],nodes[1])
-        left = nodes[0]
+        left = nodes.pop(0)
         left.position = '0'
-        right = nodes[1]
+        right = nodes.pop(0)
         right.position = '1'
-        del nodes[0]
-        del nodes[0]
         thing = (left.value[0]+right.value[0],'*')
         root = Node(thing,left,right)
         nodes.append(root)
@@ -47,9 +49,32 @@ def compress(text):
     root = create_huffman_tree(values)
     
 	# created tree, now to encode
-	
-	
-	
+    codes = {}
+    code = ''
+    # post-order traversal
+    parent_stack = []
+    current = root 
+    while len(parent_stack) > 0 or current != None:
+        if current != None:
+            parent_stack.append(current)
+            current = current.left
+            code += '0'
+        else:
+            current = parent_stack.pop()
+            code = code[:-1]
+            
+            # visit
+            print current.value
+            if current != None and current.is_leaf():
+                codes[current.value[1]] = code
+            
+            current = current.right
+            code += '1'
+    
+    # store huffman tree into code
+    for c in codes:
+        print c,codes[c]
+    
 def decompress(text):
     pass 
 
